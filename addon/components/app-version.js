5d09aaae510aff49d88c936fe9cd1151ed12e9ae
Peter Szmidt <peter.szmidt@liemak.de>
2016-01-05 10:10:04 +0100

    Split versioninformations and enable popovers in conventus

--- addon/components/app-version.js
+++ addon/components/app-version.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 import layout from '../templates/app-version';
 
 export default Ember.Component.extend({
-  tagName: 'span',
-  layout: layout
+  tagName: 'div',
+  layout: layout,
 });
--- app/components/app-version.js
+++ app/components/app-version.js
@@ -7,5 +7,35 @@ var version = config.APP.version;
 
 export default AppVersionComponent.extend({
   version: version,
-  name: name
+  name: name,
+
+  classNames: ['version-information', 'pop'],
+  attributeBindings: ['versionTitle:data-pop-title','versionMessage:data-pop-msg'],
+
+  versionTitle: 'Conventus Versionsinformationen',
+
+  pureVersion: function() {
+    if (version.split('+')[0]) {
+      return version.split('+')[0];
+    }
+    return '';
+  }.property('version'),
+
+  gitCommitHash: function() {
+    if (version.split('+')[1]) {
+      return version.split('+')[1];
+    }
+    return '';
+  }.property('version'),
+
+  versionMessage: function() {
+    let returnString = '';
+    if (this.get('pureVersion').length > 0) {
+      returnString += 'Die Installierte Conventus Version lautet: ' + this.get('pureVersion') + '▌';
+    }
+    if (this.get('gitCommitHash').length > 0) {
+      returnString += 'Der Aktuelle Git Commit Hash lautet: ' + this.get('gitCommitHash');
+    }
+    return returnString;
+  }.property('pureVersion', 'gitCommitHash'),
 });
